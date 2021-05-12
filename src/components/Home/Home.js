210b529c2939028026043a110754691382a08b5e
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-05-12 16:05:32 +0300

    Installed React Router to route through components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <div>
+      <Nav />
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
